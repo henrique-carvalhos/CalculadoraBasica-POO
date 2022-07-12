b7e301936f2f148cec3063336d5854cd76948e74
HenriqueCarvalho <henriqueart7@gmail.com>
2022-07-12 18:51:21 -0300

    Add project files.

---
+++ CalculadoraBasica_POO/Calculadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculadoraBasica_POO
+{
+    internal class Calculadora
+    {
+        public double N1 { get; private set; }
+        public double N2 { get; private set; }
+
+        public Calculadora(double n1, double n2)
+        {
+            N1 = n1;
+            N2 = n2;
+        }
+
+        public double Soma()
+        {
+            return N1 + N2;
+        }
+
+        public double Subtracao()
+        {
+            return N1 - N2;
+        }
+
+        public double Multiplicacao()
+        {
+            return N1 * N2;
+        }
+
+        public double Divisao()
+        {
+            return N1 / N2;
+        }
+    }
+}
---
+++ CalculadoraBasica_POO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculadoraBasica_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite um número: ");
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Qual operação deseja fazer?");
+            Console.WriteLine("1# Soma.");
+            Console.WriteLine("2# Subtração.");
+            Console.WriteLine("3# Multiplicação");
+            Console.WriteLine("4# Divisão.");
+            int resp = int.Parse(Console.ReadLine());
+
+            Calculadora c = new Calculadora(n1, n2);
+
+            switch (resp)
+            {
+                case 1:
+                    Console.WriteLine($"{n1} somado com {n2} é igual á " + c.Soma());
+                    break;
+                case 2:
+                    Console.WriteLine($"{n1} subtraido por {n2} é igual á " + c.Subtracao());
+                    break;
+                case 3:
+                    Console.WriteLine($"{n1} multiplicado por {n2} é igual á " + c.Multiplicacao());
+                    break;
+                case 4:
+                    Console.WriteLine($"{n1} dividico por {n2} é igual á " + c.Divisao());
+                    break;
+                default:
+                    Console.WriteLine("Operação não existente");
+                    break;
+            }
+        }
+    }
+}
